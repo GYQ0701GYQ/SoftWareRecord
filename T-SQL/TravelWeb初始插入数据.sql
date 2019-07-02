6d7016d009947ae97abf48c8d16f7e3c9d02a1eb
GYQ0701GYQ <GYQ0701GYQ@163.com>
2019-07-02 20:32:28 +0800

    数据库维护的SQL语句，demo1.0版本

---
+++ T-SQL/TravelWeb初始插入数据.sql
@@ -0,0 +1,21 @@
+INSERT INTO user_
+VALUES('342501199907018220','RainChe','17801119872','rainchekey')
+
+INSERT INTO firm
+VALUES('Fake携程','fakexiechengkey')
+
+INSERT INTO sight(sight_name,sight_loca,opentime,ticket_price,introduction)
+VALUES('黄山','中国-安徽-黄山','06:30-16:30',190,'黄山原名“黟山”，因峰岩青黑，遥望苍黛而名。后因传说轩辕黄帝曾在此炼丹，故改名为“黄山”。黄山代表景观有“四绝三瀑”，四绝：奇松、怪石、云海、温泉；三瀑：人字瀑、百丈泉、九龙瀑。黄山迎客松是安徽人民热情友好的象征，承载着拥抱世界的东方礼仪文化。')
+
+INSERT INTO route(belong_firm)
+VALUES('Fake携程')
+
+INSERT INTO visit
+VALUES(1,1)
+
+INSERT INTO item
+VALUES(1,2019-07-01)
+
+INSERT INTO order_
+VALUES(342501199907018220,1,2019-07-01)
+
